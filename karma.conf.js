331b41e23760665d5562e941f060e4c2c3019c9c
Howard Deiner <howard.deiner@deinersoft.com>
2017-06-02 09:28:40 -0400

    Initial commit.

--- karma.conf.js
+++ karma.conf.js
@@ -76,13 +76,13 @@ module.exports = function(config) {
       { pattern: appSrcBase + '**/*.ts', included: false, watched: false },
       { pattern: appBase + '**/*.js.map', included: false, watched: false },
       { pattern: testSrcBase + '**/*.ts', included: false, watched: false },
-      { pattern: testBase + '**/*.js.map', included: false, watched: false}
+      { pattern: testBase + '**/*.js.map', included: false, watched: false }
     ],
 
     // Proxied base paths for loading assets
     proxies: {
       // required for component assets fetched by Angular's compiler
-      "/app/": appAssets
+      '/app/': appAssets
     },
 
     exclude: [],
@@ -95,5 +95,5 @@ module.exports = function(config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false
-  })
-}
+  });
+};
